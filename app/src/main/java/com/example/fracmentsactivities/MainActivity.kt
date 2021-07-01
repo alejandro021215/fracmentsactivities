5098be8bed007390e74e02b87adf1d8a3657c325
Alejandro Camacho <alejandroyjaneth0202@gmail.com>
2021-07-01 01:23:43 -0500

    fracments 1

---
+++ app/src/main/java/com/example/fracmentsactivities/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.fracmentsactivities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    lateinit var textInputNombre:TextInputLayout
+    lateinit var editTextApellido:EditText
+    lateinit var btnEnviar:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textInputNombre = findViewById(R.id.TILNombre)
+        editTextApellido = findViewById(R.id.etApellido)
+        btnEnviar = findViewById(R.id.btnEnviar)
+
+
+        btnEnviar.setOnClickListener {
+            val apellido = editTextApellido.text.toString()
+            val nombre = textInputNombre.editText!!.text.toString()
+            Log.i("datos","datos ingresados: $nombre $apellido")
+
+            val i = Intent(this,SegundaActivity::class.java)
+
+            i.putExtra("name",nombre)
+            i.putExtra("lastname",apellido)
+
+            startActivity(i)
+        }
+
+
+    }
+}
